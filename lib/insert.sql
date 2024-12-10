1d75c1aabbbc59005b9a48808f90ea6953aea5ba
Melissa <melissakiptoo@gmail.com>
2024-12-10 10:02:44 +0300

    Add File

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+(1, 'Mr. Chocolate', 23, 'M', 'Dark Brown', 'INTJ', 1),
+(2, 'Rowdy', 21, 'M', 'Brown', 'ENTJ', 0),
+(3, 'Tabitha', 3, 'F', 'Dark Brown', 'INTJ', 1),
+(4, 'Sergeant Brown', 27, 'M', 'Brown', 'INTJ', 0),
+(5, 'Melissa', 16, 'F', 'White', 'ENTJ', 1),
+(6, 'Grinch', 23, 'M', 'Beige', 'INTJ', 1),
+(7, 'Wendy', 23, 'F', 'Dark Brown', 'INTJ', 0),
+(8,NULL, 1, 'M', 'Brown', 'Happy', 1);
